d000b906cfb7b420e64b0083ed9a296668a50264
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2024-10-25 13:59:04 +0700

    test

--- App.tsx
+++ App.tsx
@@ -1,23 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {useState} from 'react'
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState('Name');
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text>
+                    Hello: {name}
+                </Text>
+                <TextInput
+                    multiline={true}
+                    onChangeText={(value) => setName(value)}
+                    style={styles.text_input}
+                />
+            </View>
+            <View>
+                <Text style={styles.text}>
+                    {count}
+                </Text>
+                <Button title='Increase' onPress={()=> setCount(count + 1)}/>
+                <Button title='Decrease' onPress={()=> setCount(count - 1)}/>
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7badde',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    backgroundColor: '#e03657',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 50,
+        backgroundColor: '#e03657',
+    },
+    text_input: {
+        borderColor: 'green',
+        borderWidth: 1,
+        padding: 10,
+        width: 150
+    }
 });
