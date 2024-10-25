e2e532344ec881983b62bd9c0dbc8dc28716b3a3
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2024-10-25 12:31:53 +0700

    init app

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
