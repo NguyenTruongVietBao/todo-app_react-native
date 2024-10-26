8e09e94c1028d9e6ccdbda8c3b034fdc2654f551
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2024-10-26 18:45:33 +0700

    fix

--- Iphone13Frame.tsx
+++ Iphone13Frame.tsx
@@ -17,27 +17,17 @@ const Iphone13Frame: React.FC<Iphone13FrameProps> = ({ children }) => {
 const styles = StyleSheet.create({
   iphone13: {
     padding: 50,
-<<<<<<< HEAD
     width: 390,
     height: 844,
     backgroundColor: "#fff",
     borderRadius: 44,
-=======
-    width: 392,
-    height: 860,
-    backgroundColor: "#fff",
-    borderRadius: 48,
->>>>>>> 2b3dd484b3f6dd0bb2df551e75a7ea45a1e6318a
+
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
     borderColor: "#e0e0e0",
-<<<<<<< HEAD
-=======
-    borderWidth: 1,
->>>>>>> 2b3dd484b3f6dd0bb2df551e75a7ea45a1e6318a
     alignItems: "center",
     position: "relative",
     overflow: "hidden", // Ensures content stays inside the frame
